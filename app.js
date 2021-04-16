4b62c4f29888eb3137de92d2e040fbcf1eee4489
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-04-16 13:42:00 +0200

    removes index:false in app.use

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'), { index:false}));
+// app.use(express.static(path.join(__dirname, 'public'), { index:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', adminRouter);
 app.use('/', indexRouter);

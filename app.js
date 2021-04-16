35f90c921b24f5a31f1a3588ecfc8ae8b2076b3a
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-04-16 13:38:24 +0200

    test2

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'), { index:false}));
 
-app.use('/', adminRouter);
+// app.use('/', adminRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 

7688f9aa2f55b6895930c32aba129a2c91306759
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-04-23 09:43:30 +0200

    removes console logs

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,6 @@ router.post('/register', function(req, res) {
   //Hämta MongoDB
   req.app.locals.db.collection("users").find( {"userName":req.body.userName} ).toArray()
   .then(results => {
-    // console.log("results", results); 
 
       if ( results == "") {
 
@@ -58,16 +57,13 @@ router.post("/userpage", function(req,res) {
 
     //if kolla om det results är [] = ej hittat användare
     if (results == "") {
-      // console.log("ingen user hittad");
       res.json( {"code": "error"} )
   
     } else {
-      // console.log("pass in MongoDB is:", results[0].password);
-  
       let originalPass = CryptoJS.AES.decrypt(results[0].password, "o=B6LCgWNMXYDT(").toString(CryptoJS.enc.Utf8);
 
       if (originalPass === checkUser.password) {
-        res.json( {"code": "ok", "userId": results[0].id} ) //skickar tillb code + id
+        res.json( {"code": "ok", "userId": results[0].id} )
       } else {
         res.json( {"code": "error"} )
       }
@@ -83,10 +79,8 @@ router.get('/userpage/:id', function(req, res, next) {
   //hämta users
   req.app.locals.db.collection("users").find( {"id": showUserId} ).toArray()
   .then(results => {
-    // console.log("om user finns i databasen visas hela användarens objekt, annars []", results);
 
     if (results == "") {
-      // console.log("denna userpage finns ej");
       res.json( {"code": "error"} )
 
     } else {
@@ -103,15 +97,11 @@ router.post('/subscribe/:id', function(req, res) {
   req.app.locals.db.collection("users").find( {"userName":findUser.userName} ).toArray()
   .then(result => {
 
-    console.log("detta är findUser i databasen:", result);
-
         switch (result[0].subscription) {
           case true: 
-            console.log("ändra till false");
             result[0].subscription = false;
             break;
           case false: 
-            console.log("ändra till true");
             result[0].subscription = true;
             break;
         };
@@ -120,7 +110,6 @@ router.post('/subscribe/:id', function(req, res) {
         req.app.locals.db.collection("users").updateOne( {"id" : result[0].id}, {$set: {"subscription": result[0].subscription} }  )
         .then(result => {
         });
-        // console.log("result[0] after $set", result[0]);
 
         res.json( {"code": "Uppdaterat prenumerationsstatuset!", "subscription": result[0].subscription} ); 
   });
